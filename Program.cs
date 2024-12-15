d6eb4ced4f8c6b094a29dc2b4246384d230e86ca
Timothy M Preble <tpreble@usxpress.com>
2024-12-14 21:09:13 -0500

    some change from m1pro

--- Program.cs
+++ Program.cs
@@ -1,6 +1,38 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Serilog;
+using Serilog.Sinks.Loki; // Required for Loki integration
 
-app.MapGet("/", () => "Hello, World! From C# K3s Deployment");
+var logger = new LoggerConfiguration()
+    .WriteTo.Console() // Log to console
+    .WriteTo.LokiHttp("http://loki:3100") // Loki endpoint
+    .CreateLogger();
 
-app.Run();
+// Use Serilog as the logging provider
+Log.Logger = logger;
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add Serilog to the app's logging pipeline
+    builder.Host.UseSerilog();
+
+    var app = builder.Build();
+
+    app.MapGet("/", () =>
+    {
+        Log.Information("Received request to root endpoint");
+        return "Hello, World! From C# K3s Deployment";
+    });
+
+    logger.Information("Application starting up");
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application startup failed");
+}
+finally
+{
+    Log.Information("Application shutting down");
+    Log.CloseAndFlush();
+}
